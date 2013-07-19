7c6e18c4dc6364ef645e3a2f40a08a7af2e434a9
Brian Beckman <bc.beckman@gmail.com>
2013-07-18 17:13:35 -0700

    piddle

--- sections.hs
+++ sections.hs
@@ -1,3 +1,12 @@
+import Debug.Trace
+import Data.Set
+import Data.List
+
 divideByTen :: (Floating a) => a -> a
 divideByTen = (/ 10)
 
+lostNumbers = [4, 8, 15, 16, 23, 42]
+
+numUniques :: (Eq a) => [a] -> Int
+numUniques = length . nub
+-- primes = [2, 3] ++ ([5, 7..] `minus` unionAll [[p*p, p*p+2*p..] | p <- tail primes])
